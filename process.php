2d5d6a139858c9a6f4ca47fb243e98141913db5b
nikita shrestha <nik.stha81@gmail.com>
2019-01-13 17:10:57 +0545

    f

---
+++ process.php
@@ -0,0 +1,10 @@
+<?php
+
+print_r($_FILES); //this will print out the received name, temp name, type, size, etc.
+ 
+$input = $_FILES['audio_data']['tmp_name']; //get the temporary name that PHP gave to the uploaded file
+$output = $_FILES['audio_data']['name'].".wav"; //letting the client control the filename is a rather bad idea
+print($_POST['user']);
+//move the file from temp name to local folder using $output name
+move_uploaded_file($input, $output)
+?>
